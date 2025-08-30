350c40979677347add9d02a2bd94157f64c4decf
saif-at-scalekit <saif.shaik@scalekit.com>
2025-08-30 12:47:05 +0530

    feat: integrate cli-welcome for enhanced CLI user experience

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Hello World');
+import welcome from 'cli-welcome';
+
+welcome({
+  title: 'Locksmith CLI',
+  tagLine: 'Seamlessly add authentication infrastructure to AI applications',
+  description:
+    'A CLI tool for developers to seamlessly add authentication infrastructure to AI applications—supports modern auth providers, works with your existing tool chains or AI tools',
+  bgColor: '#36BB82',
+  color: '#000000',
+  bold: true,
+  clear: true,
+});
